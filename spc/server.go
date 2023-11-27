9cacfcab842681afca7f7b4c0fc129ee702eefca
Jone Shell <jshells97@yandex.com>
2023-11-27 18:02:59 +0300

    RM-2984:пересоздания_протофайла_для_проиводственного_календаря_для_которого_можно_скачать_правздники_добавления_редактирование_удалениея_нового_праздника

--- spc/server.go
+++ spc/server.go
@@ -12,5 +12,21 @@ type Server struct {
 
 func (c CalendarService) AddHoliday(ctx context.Context, r *pb.AddHolidayRequest) (*pb.AddHolidayResponse, error) {
 	fmt.Printf("Received request to add holiday: %s and %s\n", r.GetName(), r.GetData())
+	ctx.Err()
 	return &pb.AddHolidayResponse{HolidayName: "День к новому году", HolidayData: "31.12.2023"}, nil
+}
+
+func (c CalendarService) GetHoliday(ctx context.Context, r *pb.GetHolidayRequest) (*pb.GetHolidayResponse, error) {
+	fmt.Printf("Received request to add holiday: %s, %s and %s\n", r.GetId(), r.GetName(), r.GetData())
+	return &pb.GetHolidayResponse{HolidayName: "День к новому году", HolidayData: "31.12.2023"}, nil
+}
+
+func (c CalendarService) EditHoliday(ctx context.Context, r *pb.EditHolidayRequest) (*pb.EditHolidayResponse, error) {
+	fmt.Printf("Received request to add holiday: %s and %s\n", r.GetName(), r.GetData())
+	return &pb.EditHolidayResponse{HolidayName: "День к новому году", HolidayData: "31.12.2023"}, nil
+}
+
+func (c CalendarService) DeleteHoliday(ctx context.Context, r *pb.DeleteHolidayRequest) (*pb.DeleteHolidayResponse, error) {
+	fmt.Printf("Received request to add holiday: %s, %s and %s\n", r.GetId(), r.GetName(), r.GetData())
+	return &pb.DeleteHolidayResponse{HolidayName: "День к новому году", HolidayData: "31.12.2023"}, nil
 }
